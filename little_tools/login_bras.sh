17a0c42f4c824932a0283a856b35a51b0d27da7a
Zhou Yaoyang <diamondzyy@sina.com>
2016-11-02 11:47:36 +0800

    modified bras login

--- little_tools/login_bras.sh
+++ little_tools/login_bras.sh
@@ -1,4 +1,6 @@
-pdata="username=131220070&password="
-pdata=$pdata$1
+pdata1="username="
+pdata2="&password="
+pdata=$pdata1$1$pdata2$2
+echo $pdata
 wget -qO- http://p.nju.edu.cn/portal_io/login --post-data $pdata
 # &> /dev/null
